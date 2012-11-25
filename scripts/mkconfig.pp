13430d76650e0d92ca9538a72e81a952112f479c
Dimitar Dimitrov <mitkofr@yahoo.fr>
2012-11-25 12:14:23 +0000

    A puppet config for my own machine

---
+++ scripts/mkconfig.pp
@@ -0,0 +1,35 @@
+# diff="colordiff -Nu '%s' '%s' | less --no-init --QUIT-AT-EOF -R"
+# merge="vimdiff -c 'saveas '%s'' -c next -c 'setlocal noma readonly' -c prev '%s' '%s'"
+# replace-wscomments=yes
+# replace-unmodified=yes
+file { '/etc/dispatch-conf.conf':
+  source => 'puppet://path',
+  mode   => mode,
+}
+
+# Defaults:mitko timestamp_timeout=560 # min before asking for password again
+file { '/etc/sudoers':
+  source => 'puppet://path',
+  mode   => mode,
+}
+
+# git config --global user.name 'Dimitar Dimitrov'
+# git config --global user.email 'mitkofr@yahoo.fr'
+# git config --global color.ui true
+
+package { 'app-editors/gvim':
+  ensure => present,
+}
+
+package { 'net-print/cups':
+  ensure => present,
+}
+
+service { 'cupsd':
+  ensure     => stopped,
+  enable     => false,
+  hasrestart => true,
+  hasstatus  => true,
+}
+
+Package['net-print/cups'] -> Service['cupsd']
